e56106030e48d407e12250ca930b981a7fa5fe73
cristianworth <cristianweissmantel@gmail.com>
2023-12-16 19:05:53 -0300

    Revert "Added userId in the return of login method"
    This reverts commit 4cde703c52a2f8d96a56c3302493bd2554dbd388.

--- src/auth/login.ts
+++ src/auth/login.ts
@@ -1,67 +1,41 @@
-import { type Request, type Response } from "express";
-import jwt from "jsonwebtoken";
-import { Autenticaveis } from "./authEntity.js";
+import { type Request, type Response } from 'express'
+import jwt from 'jsonwebtoken'
+import { Autenticaveis } from './authEntity.js'
 
-import { AppDataSource } from "../data-source.js";
-import { decryptPassword } from "../utils/senhaUtils.js";
-import { AppError } from "../error/ErrorHandler.js";
+import { AppDataSource } from '../data-source.js'
+import { decryptPassword } from '../utils/senhaUtils.js'
+import { AppError } from '../error/ErrorHandler.js'
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-  const { email, senha } = req.body;
+  const { email, senha } = req.body
 
   const autenticavel = await AppDataSource.manager.findOne(Autenticaveis, {
-    select: ["id", "rota", "role", "senha"],
-    where: { email },
-  });
+    select: ['id', 'rota', 'role', 'senha'],
+    where: { email }
+  })
 
   if (autenticavel == null) {
-    throw new AppError("Não encontrado!", 404);
+    throw new AppError('Não encontrado!', 404)
   } else {
-    const { id, rota, role, senha: senhaAuth } = autenticavel;
-    const senhaCorrespondente = decryptPassword(senhaAuth);
+    const { id, rota, role, senha: senhaAuth } = autenticavel
+    const senhaCorrespondente = decryptPassword(senhaAuth)
 
     if (senha !== senhaCorrespondente) {
-      throw new AppError("Senha incorreta!", 401);
+      throw new AppError('Senha incorreta!', 401)
     }
 
-    const token = signToken(id, role);
-    res.status(200).json(token);
-  }
-};
+    const token = jwt.sign({ id, role }, process.env.SECRET, {
+      expiresIn: 86400
+    }) // expira em 24 horas
 
-function signToken(id: string, role: string) {
-  interface TokenProps {
-    id: string;
-    role: string;
-  }
-
-  const payload: TokenProps = { id: id, role: role };
-  const secretKey: string = process.env.SECRET || "";
-  console.log("secretKey = ", secretKey);
-
-  const token = jwt.sign(payload, secretKey, { expiresIn: "24h" });
-  console.log("token generated = ", token);
-
-  try {
-    const decodedToken: TokenProps = jwt.verify(
+    res.status(200).json({
+      auth: true,
       token,
-      process.env.SECRET
-    ) as TokenProps;
-    console.log("Decoded Token:", decodedToken);
-
-    const userId = decodedToken.id;
-    
-    // TODO: Esse decode não funciona no repositório https://github.com/cristianworth/voll-mobile
-    // Como não é possivel decodificar o token, eu passo o userId já decodificado no retorno
-    // Posteriormente terá que retornar somente o token codificado: return { token };
-    return { token, userId };
-  } catch (error) {
-    console.error("Error decoding token:", error.message);
+      rota
+    })
   }
-
-  return { token };
 }
 
 export const logout = async (req: Request, res: Response): Promise<void> => {
-  res.status(200).json({ auth: false, token: null });
-};
+  res.status(200).json({ auth: false, token: null })
+}
